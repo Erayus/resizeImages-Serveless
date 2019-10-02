64520571a0104fe3c68677e010add3455ed7bf74
Erayus <raymondhieutran@gmail.com>
2019-10-02 14:52:18 +1000

    [New] Resize the file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const uuidv4 = require('uuid/v4');
 const {promisify} = require('util');
 const AWS = require('aws-sdk');
 
+// Convert resize function im.image to a async function which is used with await below
+const resizeAsync = promisify(im.resize);
+
 AWS.config.update({region: 'ap-southeast-2'});
 const s3 = new AWS.S3();
 
@@ -19,9 +22,20 @@ exports.handler = async (event) => {
             Bucket: bucket,
             Key: filename
         }
+        let inputData = await s3.getObject(params).promise();
+
         // Resize the file
+        let tempFile = os.tempdir() + '/' + uuidv4() + '.jpg';
+        let resizeArgs = {
+            srcData: inputData.Body,
+            dstPath: tempFile,
+            width: 150
+        };
+        await resizeAsync(resizeArgs);
 
         // Read the resized file
+
+        
     });
 
     await Promise.all(filesProcessed);
