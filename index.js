a97db4c063a3efc2452b07f8a85baadf881bede1
Erayus <raymondhieutran@gmail.com>
2019-10-02 14:47:08 +1000

    [New] Get file from S3

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const im = require('imagemagick');
 const fs = require('fs');
-const os = require()
+const os = require('os');
+const uuidv4 = require('uuid/v4');
+const {promisify} = require('util');
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'ap-southeast-2'});
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+
+    let filesProcessed = event.Records.map( async (record) => {
+        let bucket = record.s3.bucket.name;
+        let filename = record.s3.object.key;
+
+        // Get file from S3
+        var params = {
+            Bucket: bucket,
+            Key: filename
+        }
+        // Resize the file
+
+        // Read the resized file
+    });
+
+    await Promise.all(filesProcessed);
+    console.log("done");
+    return "done"
+}
